75f4d0c04727c6f833e8a107f5f607dd3efc96fd
robert <robert.langereis@gmail.com>
2020-02-25 13:42:45 +0100

    initial setup

---
+++ src/app/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Hello!</p>
+    <p>I'm a user component</p>
+  `
+})
+export class UserComponent {}
